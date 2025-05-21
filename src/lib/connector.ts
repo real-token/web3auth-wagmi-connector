675a998f61517916dd36356fa10f25fb7e57fc7c
chichke <bastien.silhol@epitech.eu>
2025-05-21 15:08:44 +0200

    fix: eslint noconsole

--- src/lib/connector.ts
+++ src/lib/connector.ts
@@ -23,6 +23,7 @@ export function Web3AuthConnector(parameters: Web3AuthConnectorParams) {
     name: name || "Web3Auth",
     type: type || "Web3Auth",
     async connect({ chainId, loginHint }: { chainId?: number; loginHint?: string } = {}) {
+      // eslint-disable-next-line no-console
       console.log("connect", chainId, loginHint);
       try {
         config.emitter.emit("message", {
